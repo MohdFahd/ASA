6e804aaa35d79dca0b1833d0b40b84cee40b4e17
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-05-07 16:01:51 +0300

    Update ASA

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#13120C",
+        secondary: "#F0B624",
+      },
+    },
+    fontSize: {
+      sm: "12px",
+      base: "14px",
+      xl: "17px",
+      lg: "16px",
+      "2xl": "22px",
+      "3xl": "28px",
+      "4xl": "34px",
+      "5xl": "46px",
+    },
+  },
+  daisyui: {
+    themes: ["light"],
+  },
+  plugins: [require("daisyui")],
+};
